c9ad672e503f197a1a866271a9f69be786618d4e
ghsr <ghsr92@yandex.ru>
2016-10-12 00:14:33 +0600

    i9105p: Remove full_i9105p.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_i9105p.mk \
-    $(LOCAL_DIR)/full_i9105p.mk
+    $(LOCAL_DIR)/aosp_i9105p.mk
--- aosp_i9105p.mk
+++ aosp_i9105p.mk
@@ -1,8 +1,18 @@
-# Inherit device configuration
-$(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# Device identifier. This must come after all inclusions
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/i9105p/i9105p.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := i9105p
 PRODUCT_NAME := aosp_i9105p
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-I9105P
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
